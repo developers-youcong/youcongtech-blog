b20ab82a23369b0bf2e16f6e337fdb2465018901
developers-youcong <15200740587@163.com>
2019-06-11 19:45:05 +0800

    嵌入iframe

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('extension.blog', () => {
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel(
+        'catCoding',
+        '我的博客',
+        vscode.ViewColumn.One,
+        {}
+      );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "youcongtech-blog" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+      // And set its HTML content
+	  panel.webview.html = getWebviewContent();
+	  
+    })
+  );
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+<iframe src="https://developers-youcong.github.io/" width="900" height="900"/>
+
+
+</body>
+</html>`;
+
+}
